bf008c04eef25b8b214382d24079b2ca8f623b91
Kasam Revan Sai Karthikeya <karthikeya.kasam@gmail.com>
2026-07-05 20:10:59 +0530

    Time: 843 ms (74.16%), Space: 0B (100%) - LeetHub

---
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+with cte as (
+    select u.name, ifnull(sum(r.distance),0) as travelled_distance  from users as u
+    left join rides as r
+    on u.id=r.user_id
+    group by u.id
+)select name, travelled_distance from cte
+order by travelled_distance desc , name asc;
