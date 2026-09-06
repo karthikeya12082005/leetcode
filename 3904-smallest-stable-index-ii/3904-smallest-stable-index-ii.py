7c320e8ada91f73769154dc19f077898d964e8f6
Kasam Revan Sai Karthikeya <karthikeya.kasam@gmail.com>
2026-09-06 10:09:37 +0530

    Time: 83 ms (98.09%), Space: 32.4 MB (66.61%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
@@ -1,11 +1,19 @@
 class Solution:
-    def firstStableIndex(self, A: List[int], k: int) -> int:
-        msf = -1
-        cand = cm = 0
-        for i, x in enumerate(A):
-            msf = max(msf, x)
-            if i == cand:
-                cm = msf
-            if x < cm - k:
-                cand = i + 1
-        return cand if cand < len(A) else -1
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+        mini = [0] * n
+
+        mint = float('inf')
+        for i in range(n - 1, -1, -1):
+            if nums[i] < mint:
+                mint = nums[i]
+            mini[i] = mint
+
+        maxt = 0
+        for i in range(n):
+            if nums[i] > maxt:
+                maxt = nums[i]
+            if maxt - mini[i] <= k:
+                return i
+
+        return -1
